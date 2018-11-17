0156b94c2699701e36f644f8cd63b5532042ebca
mohith <mohithadiboina95@gmail.com>
2018-11-18 01:21:25 +0530

    email verification is done

--- ASE_Project/settings.py
+++ ASE_Project/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS =[
      os.path.join(BASE_DIR,'static')
 ]
 
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'aseproject321@gmail.com'
 EMAIL_HOST_PASSWORD = 'ucandoit456'
 EMAIL_PORT = 587
--- Customers/urls.py
+++ Customers/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
     path('signup/', views.signup, name='Cus_signup'),
     path('categories/', views.categories, name='Cus_categories'),
     path('logout/', views.user_logout, name='logout'),
-    path('res_area/',views.restaurants,name='res_area')
+    path('res_area/',views.restaurants,name='res_area'),
+    re_path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
