a3ec5dc924ba2776eb4b1f3b844184270e819fff
SatishKumar9 <satishkumarnarava9@gmail.com>
2018-11-18 19:15:45 +0530

    restaurant details

--- Delivery/views.py
+++ Delivery/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from Customers.models import Order, Address
+from .models import Delivery
 from django.http import HttpResponse
+
 
 def index(request):
     return render(request, 'Delivery/index.html')
 
+
 def signup(request):
-    return render(request, 'Delivery/signup.html')
+    return render(request, 'Delivery/signup.html',)
