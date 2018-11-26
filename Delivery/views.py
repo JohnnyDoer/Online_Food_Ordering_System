68a018ae9076002422f9615c0bf29a7ebbe106bf
mohith <mohithadiboina95@gmail.com>
2018-11-26 21:29:04 +0530

    delivery _ Email & reset password

--- Delivery/views.py
+++ Delivery/views.py
@@ -4,6 +4,13 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import DeliveryGuyProfileInfoForm, SignUpForm
 from .models import Delivery
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -29,11 +36,23 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            return redirect('http://127.0.0.1:8000/delivery')
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('Delivery/emailver.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
         else:
             context = {'form': form, }
             return render(request, 'Delivery/signup.html', context=context)
@@ -58,4 +77,17 @@ def profile_page(request):
         profile_form = DeliveryGuyProfileInfoForm()
         context = {'Profile_form': profile_form}
     return render(request, 'Delivery/profile.html', context=context)
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return render(request, 'Delivery/After_Activation.html')
+    else:
+        return HttpResponse('Activation link is invalid!')
 
