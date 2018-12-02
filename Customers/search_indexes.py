cdc617123989bb6bb54c58a67f90f5fd3cbc23b1
Swathi-Sindhu <harshithachowdary.t17@iiits.in>
2018-12-02 20:05:11 +0530

    Search Engine added with modifications

--- Customers/search_indexes.py
+++ Customers/search_indexes.py
@@ -2,18 +2,6 @@ from haystack import indexes
 from .models import Restaurant, FoodCategory, Food
 
 
-class RestaurantIndex(indexes.ModelSearchIndex, indexes.Indexable):
-    class Meta:
-        model = Restaurant
-        excludes = ['Restaurant_ID', 'user', 'Restaurant_Logo', 'Restaurant_Phone_Number', 'Restaurant_Email',
-                    'Restaurant_Regex', 'Restaurant_Pin']
-
-    def get_model(self):
-        return Restaurant
-
-    def index_queryset(self, using=None):
-        return self.get_model().objects.all()
-
 
 class FoodIndex(indexes.ModelSearchIndex, indexes.Indexable):
     class Meta:
